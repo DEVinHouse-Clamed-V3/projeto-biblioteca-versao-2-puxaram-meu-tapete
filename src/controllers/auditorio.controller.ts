4de307b2c96f562ea9e743daa1bdb1d7d5f572d1
frankberwald <berwald1997@outlook.com>
2025-02-02 18:01:53 -0300

    identa auditorio

--- src/controllers/auditorio.controller.ts
+++ src/controllers/auditorio.controller.ts
@@ -11,7 +11,7 @@ export class AuditorioControllers {
   }
 
   auditoriumCreate = async (req: Request, res: Response) => {
-    try{
+    try {
       const body = req.body;
       console.log("Requisição recebida:", req.body);
       if (!body.name) {
@@ -22,19 +22,19 @@ export class AuditorioControllers {
         return res.status(400).json({ error: 'A localização é obrigatório' })
       } else if (!body.has_projector === undefined) {
         return res.status(400).json({ error: 'Essa informação é obrigatória' })
-      }else if(!body.has_sound_system === undefined){
+      } else if (!body.has_sound_system === undefined) {
         return res.status(400).json({ error: 'Essa informação é obrigatória' })
       } else {
         console.log("Tentando salvar o auditório:", body);
         const auditorium = await this.auditorioRepository.save(body)
         console.log("Auditório salvo com sucesso:", auditorium);
         return res.status(201).json(auditorium)
+      }
+    } catch (error) {
+      console.log("Erro ao registrar dados", error)
+      return res.status(500).json({ error: "Erro interno" })
     }
-  } catch (error){
-    console.log("Erro ao registrar dados", error)
-    return res.status(500).json({error: "Erro interno"})
   }
-}
 
   auditoriumGetAll = async (req: Request, res: Response) => {
     try {
@@ -47,60 +47,61 @@ export class AuditorioControllers {
   }
 
   auditoriumGetById = async (req: Request, res: Response) => {
-    try{
+    try {
       const params = req.params;
       const auditoriumsInDB = await this.auditorioRepository.findOneBy({
         id: parseInt(params.id)
       })
-      if(!auditoriumsInDB) {
-        res.status(404).json({error: "Registro não encontrado"})
+      if (!auditoriumsInDB) {
+        res.status(404).json({ error: "Registro não encontrado" })
       } else {
         res.json(auditoriumsInDB)
       }
-    }catch(error){
+    } catch (error) {
       console.error("Erro ao recuperar dados")
-      return res.status(500).json({message: "Erro interno do servidor"})
+      return res.status(500).json({ message: "Erro interno do servidor" })
     }
   }
 
   updateAuditorium = async (req: Request, res: Response) => {
-    try{
+    try {
       const params = req.params;
       const body = req.body;
       const auditoriumsInDB = await this.auditorioRepository.findOneBy({
         id: parseInt(params.id)
       });
-      if(!auditoriumsInDB){
-        res.status(404).json({error: "Registro não encontrado"})
+      if (!auditoriumsInDB) {
+        res.status(404).json({ error: "Registro não encontrado" })
       } else {
         Object.assign(auditoriumsInDB, body)
         await this.auditorioRepository.save(auditoriumsInDB)
         res.status(200).json(auditoriumsInDB)
       }
-    }catch(error){
-      res.status(500).json({message :"Não foi possível atualizar registro"})
+    } catch (error) {
+      res.status(500).json({ message: "Não foi possível atualizar registro" })
     }
   }
 
   deleteAuditorium = async (req: Request, res: Response) => {
-    try{
+    try {
       const params = req.params;
       const auditoriumsInDB = await this.auditorioRepository.findOneBy({
         id: parseInt(params.id)
       })
-      if (!auditoriumsInDB){
+      if (!auditoriumsInDB) {
         res.status(404).json({ error: "Registro não encontrado" });
       } else {
         await this.auditorioRepository.delete(auditoriumsInDB.id);
-        res.status(204).json();      }
+        res.status(204).json();
+      }
 
-    }catch(error){
-      res.status(500).json({error: "Não foi possível deletar o registro"})
+    } catch (error) {
+      res.status(500).json({ error: "Não foi possível deletar o registro" })
     }
   }
 
   searchCompleteAuditoriums = async (req: Request, res: Response) => {
-    try{
+    try {
       const params = req.param
       const auditoriumsInDB = await this.auditorioRepository.find({
         where: {
@@ -110,9 +111,9 @@ export class AuditorioControllers {
         },
       })
       return res.status(200).json(auditoriumsInDB)
-    }catch(error){
+    } catch (error) {
       console.error("Erro ao buscar resultados", error);
-      return res.status(500).json({message: "Erro interno do servidor"})
+      return res.status(500).json({ message: "Erro interno do servidor" })
     }
   }
 }
