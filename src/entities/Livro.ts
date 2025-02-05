695a3b005fda614e83da5166e76319c8d64d154d
Gustavo Fernandes <gusstavoffc@gmail.com>
2025-02-04 23:34:39 -0300

    Criando entidade livro

--- src/entities/Livro.ts
+++ src/entities/Livro.ts
@@ -1,6 +1,35 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-class Livro {
- 
+@Entity()
+class Livro{
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    publication_date: Date;
+
+    @Column()
+    isbn: string;
+
+    @Column()
+    page_count: number;
+
+    @Column()
+    language: string;
+
+    @Column()
+    created_at: Date;
+   
+    @Column()
+    updated_at: Date;
+
 }
 
 export default Livro;
